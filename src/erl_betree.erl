fa7a5bde0613dd4bcc16ffbb7ba5961c800c63c6
vs-ads <127510729+vs-ads@users.noreply.github.com>
2024-01-14 17:21:07 -0500

    betree_search_yielding/3

--- src/erl_betree.erl
+++ src/erl_betree.erl
@@ -6,7 +6,8 @@
     betree_insert_sub/2,
     betree_exists/2,
     betree_search/2,
-    betree_search/3
+    betree_search/3,
+    betree_search_yielding/3
 ]).
 
 -inline([check_clock_type/1]).
@@ -36,3 +37,19 @@ check_clock_type(?CLOCK_REALTIME) -> ?CLOCK_REALTIME;
 check_clock_type(?CLOCK_PROCESS_CPUTIME_ID) -> ?CLOCK_PROCESS_CPUTIME_ID;
 check_clock_type(?CLOCK_THREAD_CPUTIME_ID) -> ?CLOCK_THREAD_CPUTIME_ID;
 check_clock_type(_) -> ?CLOCK_MONOTONIC.
+
+
+% Implementation of betree_search that
+% CONDITIONALLY yields control back to Erlang scheduler during the search
+% subject to Timeslice parameter:
+%   if Timeslice <= 0, then
+%       NO, control will NOT be yielded to Erlang scheduler during the search;
+%   if Timeslice >= 100, then
+%       YES, control will be yielded to Erlang scheduler during the search;
+%   0 < Timeslice < 100, then
+%       yielding control to Erlang scheduler during the search
+%       DEPENDS on enif_consume_timeslice return value:
+%           the closer Timeslice to 100, then more probable that
+%           enif_consume_timeslice return value from Erlang RTS.
+betree_search_yielding(Betree, Event, Timeslice) ->
+    erl_betree_nif:betree_search_yielding(Betree, Event, Timeslice).
