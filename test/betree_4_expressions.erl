114f1594f737ec0955d0b1b9a8d44e838c646617
Victor Savkov <v.savkov@samsung.com>
2024-04-02 22:29:25 -0400

    added four_expressions_case_1_test, four_expressions_case_2_test

--- test/betree_4_expressions.erl
+++ test/betree_4_expressions.erl
@@ -2,7 +2,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-four_expressions_test() ->
+four_expressions_case_1_test() ->
   Params = [
     {p1, bool, disallow_undefined},
     {p2, bool, disallow_undefined},
@@ -52,3 +52,57 @@ four_expressions_test() ->
   ?assert(is_list(Ids)),
 %%  ?assertEqual(4, length(Ids)),
   ?assertEqual([Id1, Id2, Id3, Id4], lists:sort(Ids)).
+
+four_expressions_case_2_test() ->
+  Params = [
+    {p1, bool, disallow_undefined},
+    {p2, bool, disallow_undefined},
+    {p3, bool, disallow_undefined}
+  ],
+
+  Event = [{bool_event, false, true, false}],
+  % i.e. p1 = false, p2 = true, p3 = false
+
+  Expr1 = <<"p2 and (p3 or p1)">>,
+  % manual calculation:
+  % p2 and (p3 or p1) = true and (false or false) = false
+  Id1 = 101,
+
+  Expr2 = <<"p1 or (p2 or p3)">>,
+  % manual calculation:
+  % p1 or (p2 or p3) = false or (true or false) = true
+  Id2 = 202,
+
+  Expr3 = <<"(p1 and p2) or p3">>,
+  % manual calculation:
+  % (p1 and p2) or p3 = (false and true) or false = false
+  Id3 = 303,
+
+  Expr4 = <<"(not ((not p1) and p3)) and p2">>,
+  % manual calculation:
+  % (not ((not p1) and p3)) and p2 =
+  % (not ((not false) and false)) and true =
+  % (not (true and false)) and true =
+  % (not false) and true = true
+  Id4 = 404,
+
+  {ok, Betree} = erl_betree:betree_make([Params]),
+
+  {ok, Sub1} = erl_betree:betree_make_sub(Betree, Id1, [], Expr1),
+  ok = erl_betree:betree_insert_sub(Betree, Sub1),
+
+  {ok, Sub2} = erl_betree:betree_make_sub(Betree, Id2, [], Expr2),
+  ok = erl_betree:betree_insert_sub(Betree, Sub2),
+
+  {ok, Sub3} = erl_betree:betree_make_sub(Betree, Id3, [], Expr3),
+  ok = erl_betree:betree_insert_sub(Betree, Sub3),
+
+  {ok, Sub4} = erl_betree:betree_make_sub(Betree, Id4, [], Expr4),
+  ok = erl_betree:betree_insert_sub(Betree, Sub4),
+
+  Ret = erl_betree:betree_search(Betree, Event),
+  ?assertMatch({ok, _}, Ret),
+  {ok, Ids} = Ret,
+  ?assert(is_list(Ids)),
+%%  ?assertEqual(4, length(Ids)),
+  ?assertEqual([Id2, Id4], lists:sort(Ids)).
